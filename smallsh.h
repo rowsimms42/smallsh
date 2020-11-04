577dcbb0a075f1dd7345729b4dcc2e3559d50a1e
Rowan Simmons <59707215+rowsimms42@users.noreply.github.com>
2020-11-03 21:56:38 -0500

    Update smallsh.h

--- smallsh.h
+++ smallsh.h
@@ -1,16 +1,14 @@
+/*header file for smallsh.c*/
+
 #ifndef SMALLSH_H
 #define SMALLSH_H
-#include <signal.h>
-#include <sys/types.h>
-
 
 void return_status(int);
 void handle_SIGTSTP(int);
-void handle_SIGINT(int);
 int built_in_comm(char*, char**, char*);
 int check_background(char**, int);
 int parse_input(char*, char*, char*, char**);
-void expand_variable(const char*, char*, unsigned int);
+void expand_variable(const char*, char*);
 void loop();
 void kill_background_processes(pid_t*, const int*);
 void check_background_processes(pid_t*, int*);
